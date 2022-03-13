32e6f7e459e007fe4d6671053065bc980ca226f5
RalphP <rp990@mailbox.org>
2022-03-13 23:03:52 +0100

    Fix gitignore

---
+++ src/config.h
@@ -0,0 +1,26 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "hexviewer"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "rp990@mailbox.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "HexViewer"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "HexViewer 0.4"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "hexviewer"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.4"
+
+/* Version number of package */
+#define VERSION "0.4"
